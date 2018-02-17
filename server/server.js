c5dcf0bc723277d7ab882c516074e4789ef3b4e6
moshe malka <moshemalka@moshes-mbp.home>
2018-02-18 01:22:36 +0200

    init

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const express = require('express');
+var app = express();
+
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(publicPath + '/index.html');
+// });
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}.`);
+});
