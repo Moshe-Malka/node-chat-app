01e3047585cbad5b1deb89ef2a2019d242fa7512
moshe malka <moshemalka2014@gmail.com>
2018-02-21 00:54:25 +0200

    add geolocation support via geolocation API

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -17,7 +17,10 @@ io.on('connection', (socket) => {
 
   socket.emit('newMessage',generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  // socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  socket.on('createLocationMessage', (coords) => {
+      io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,30 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var from = 'Jen';
     var text = 'Some message';
     var message = generateMessage(from, text);
-    expect(message.createdAt).toBeA('number');
-    expect(message).toInclude({from, text});
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toHaveProperty('from');
+    expect(message).toHaveProperty('text');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+      var from = 'moshe';
+      var longitude = 12;
+      var latitude = 56;
+      var url = 'https://ww.googlw.com/maps?q=12,56';
+      var message = generateLocationMessage(from, latitude, longitude);
+
+      expect(message).toHaveProperty('from');
+      expect(message).toHaveProperty('url');
+      expect(typeof message.createdAt).toBe('number');
+      expect(typeof longitude).toBe('number');
+      expect(typeof latitude).toBe('number');
+      expect(typeof from).toBe('string');
   });
 });
