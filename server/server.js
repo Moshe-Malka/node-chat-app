8d54df52121119a36b64599893ea2fd3cb3914f0
moshe malka <moshemalka2014@gmail.com>
2018-02-21 01:18:39 +0200

    add css for chat page

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ io.on('connection', (socket) => {
 
   socket.emit('newMessage',generateMessage('Admin', 'Welcome to the chat app'));
 
-  // socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
   socket.on('createLocationMessage', (coords) => {
       io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
@@ -25,7 +24,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
+    callback();
   });
 
   socket.on('disconnect', () => {
